23144413abf768a657d2c5ba835c449a9f7e9c67
MgShepherd <michaelshep1504@gmail.com>
2025-10-11 19:51:46 +0100

    Solved problem 4 part 2

--- aoc/problems/problem4.py
+++ aoc/problems/problem4.py
@@ -11,18 +11,24 @@ class Problem4(Problem):
     def solve(self, part: int) -> int:
         num_valid = 0
         for i in range(PROBLEM_RANGE_START, PROBLEM_RANGE_END):
-            if is_valid(i):
+            if is_valid(i, part):
                 num_valid += 1
         return num_valid
 
 
-def is_valid(password: int) -> bool:
+def is_valid(password: int, part: int) -> bool:
     digits = [int(digit) for digit in str(password)]
     has_adjacent = False
     for i in range(0, len(digits) - 1):
         if digits[i] > digits[i + 1]:
             return False
-        elif digits[i] == digits[i + 1]:
+        elif digits[i] == digits[i + 1] and (
+            part != 2
+            or (
+                (i == 0 or digits[i] != digits[i - 1])
+                and (i >= len(digits) - 2 or digits[i] != digits[i + 2])
+            )
+        ):
             has_adjacent = True
 
     return has_adjacent
